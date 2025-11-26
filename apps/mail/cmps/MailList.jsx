5289fdc5b77e10c36f3bb91b63d9defbd3c3a646
yaelmtzk <jessianeroque@gmail.com>
2025-11-26 15:47:09 +0200

    Create Mail App Basic Structre Set service & data model & basic index, list & preview

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,3 +1,19 @@
-export function MailList() {
-    return <div>Mail list</div>
+import { MailPreview } from "MailPreview.jsx"
+
+export function MailList({ loadingClass, mails }) {
+
+    if (!mails.length) return <div>No Mails To Show...</div>
+
+
+    return(
+        <ul  className="mail-list container">
+    {
+        mails.map(mail => (
+            <li className={loadingClass} key={mail.id}>
+                <MailPreview mail={mail} />
+            </li>
+        ))
+    }
+        </ul >
+    )
 }
--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,5 +1,36 @@
+import { MailList } from "../cmps/MailList.jsx"
+import { mailService } from "../services/mail.service.js"
+
+const { useState, useEffect } = React
+const { Link, useSearchParams } = ReactRouterDOM
 
 export function MailIndex() {
-    return <div>mail app</div>
+
+    const [mails, setMails] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        loadMails()
+    }, [])
+
+    function loadMails() {
+        mailService.query()
+            .then(mails => setMails(mails))
+            .catch(err => {
+                console.log('err:', err)
+                showErrorMsg('Cannot get mails!')
+            })
+    }
+
+    if (!mails) return <div className="loader">Loading...</div>
+    console.log('mails:', mails)
+    const loadingClass = isLoading ? 'loading' : ''
+
+    return (
+        <section className="mail-index">
+            <MailList loadingClass={loadingClass} mails={mails}/>            
+        </section>
+
+    )
 }
 
