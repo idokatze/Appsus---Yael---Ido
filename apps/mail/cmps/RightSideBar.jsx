08af6317b64eaeecf83d6d6ea2b2194f8477fa64
yaelmtzk <jessianeroque@gmail.com>
2025-11-28 01:11:41 +0200

    Style mails details

--- apps/mail/cmps/RightSideBar.jsx
+++ apps/mail/cmps/RightSideBar.jsx
@@ -29,28 +29,33 @@ export function RightSideBar({ defaultFilter, onSetFilter }) {
                 <i className="fa-solid fa-pencil"></i>Compose</div>
 
             <div className="side-nav-btn inbox-btn"
-                onClick={() => handleStatusChange('inbox')}>
+                onClick={() => handleStatusChange('inbox')}
+                title="Inbox">
                 <i className="fa-solid fa-inbox"></i>
                 Inbox
             </div>
 
             <div className="side-nav-btn starred-btn"
+            title="Starred"
                 onClick={onShowStarred}>
                 <i className="fa-regular fa-star"></i>
                 Starred
             </div>
 
             <div className="side-nav-btn sent-btn"
+            title="Sent"
                 onClick={() => handleStatusChange('sent')}>
                 <i className="fa-regular fa-paper-plane"></i>
                 Sent
             </div>
 
             <div className="side-nav-btn draft-btn"
+            title="Draft"
                 onClick={() => handleStatusChange('draft')}>
                 <i className="fa-regular fa-note-sticky"></i>Draft</div>
 
             <div className="side-nav-btn trash-btn"
+            title="Bin"
                 onClick={() => handleStatusChange('trash')}>
                 <i className="fa-regular fa-trash-can"></i>Bin</div>
         </section>
--- apps/mail/pages/MailDetails.jsx
+++ apps/mail/pages/MailDetails.jsx
@@ -6,26 +6,33 @@ export function MailDetails({ mail, onBack }) {
     const { name, sentAt, subject, body, from } = mail
 
     return (
-        <section className="mail-details-screen">
-
+        <section className="mail-details-container">
+            <div className="actions-btns">
+                <button className="go-back-btn"
+                title="Back to Inbox"
+                onClick={onBack}>
+                    <i className="fa-solid fa-arrow-left"></i>
+                </button>
+            </div>
             <div className="mail-subject">{subject}</div>
 
             <div className="mail-main">
-                <img src="avatar.jpg" alt="Sender picture" className="avatar" />
+                <div className="avatar-container">
+                    <img src="../../../assets/img/avatar.png" 
+                    alt="Sender picture" className="avatar" />
+                </div>
 
-                <div className="mail-details">
+                <div className="mail-details-box">
 
-                    <div className="mail-header">
+                    <div className="mail-details-header">
 
                         <div className="sender-details">
-                            <div>
                                 <div className="sender-name">{name}</div>
-                                <div className="sender-email">&lt;{from}&gt;</div>                                
-                            </div>
-
-                            <time className="mail-date" dateTime=""> {sentAt}</time>
-
+                                <div className="sender-mail">&lt;{from}&gt;</div>                                
                         </div>
+
+                        <time className="mail-date" dateTime=""> {sentAt}</time>
+
                     </div>
 
                     <article className="mail-body">{body}</article>
