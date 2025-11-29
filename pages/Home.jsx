6dd513511f33bcfad739d043c0a39f319fdca7c8
Ido Katzenellenbogen <idokatze@gmail.com>
2025-11-30 01:17:51 +0200

    Commit - Add Home Page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,25 @@
-
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to home page!</h1>
-    </section>
+    return (
+        <section className="home">
+            <h1>Welcome</h1>
+            <p>Replicating Google Keep & Gmail with React</p>
+
+            <div className="home-sites">
+                <a href="/#/note" className="site-card">
+                    <img src="/img/keep.jpg" alt="Google Keep" />
+                    <span>Notes</span>
+                </a>
+                <a href="/#/mail" className="site-card">
+                    <img src="/img/gmail.jpg" alt="Gmail" />
+                    <span>Mail</span>
+                </a>
+            </div>
+
+            <div className="about-link">
+                <a href="/#/about" className="btn">
+                    Read About the Project
+                </a>
+            </div>
+        </section>
+    )
 }
